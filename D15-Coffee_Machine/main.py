c234d484fba73d326af1435a8f9407364aa67592
jacquelineyin <yin.sangxu@gmail.com>
2021-06-03 21:54:28 -0700

    d15: rename folders

---
+++ D15-Coffee_Machine/main.py
@@ -0,0 +1,57 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+money = 0
+
+def print_report():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money: 2f}")
+
+
+def handle_action(action):
+    if action == "off":
+        return False
+    elif action == "report":
+        print_report()
+        return True
+
+
+def coffee_machine():
+    is_on = True
+
+    while (is_on):
+        action = input(f"  What you would like? (espresso/latte/cappucino: ").lower()
+
+
+    return
